e57a4d2133bcedf533eac226710037426b31614f
Anuj Upreti <anuj.upreti.0@gmail.com>
2025-05-04 10:18:07 +0530

    Edited botData responses to include random greetings and dynamic date

--- js/botData.js
+++ js/botData.js
@@ -1,19 +1,25 @@
 // js/botData.js
+import { getRandomGreeting, getTodayDate } from './utils.js';
 
 export const botData = [
-  { keywords: ["fees", "fee", "cost"], 
-    answer: "You can check the course fees in the Admission section or Prospectus." 
+  {
+    keywords: ["fees", "fee", "cost"],
+    answer: () => `${getRandomGreeting()} You can check the course fees in the Admission section or Prospectus.`
   },
-  { keywords: ["assignment dates", "assignment submission", "submit assignment"], 
-    answer: "Last date to submit the assignment is 28 April for June 2025." 
+  {
+    keywords: ["assignment dates", "assignment submission", "submit assignment"],
+    answer: () => `${getRandomGreeting()} Last date to submit the assignment is 28 April for June 2025.`
   },
-  { keywords: ["exams", "exam dates", "exam schedule"], 
-    answer: "Exams will start from 2 June 2025." 
+  {
+    keywords: ["exams", "exam dates", "exam schedule"],
+    answer: () => `${getRandomGreeting()} Exams will start from 2 June 2025.`
   },
-  { keywords: ["admission july session", "admission", "apply july"], 
-    answer: "Admissions for July session open on 10 July 2025." 
+  {
+    keywords: ["admission july session", "admission", "apply july"],
+    answer: () => `${getRandomGreeting()} Admissions for July session open on 10 July 2025.`
   },
-  { keywords: ["today", "date", "today's date"], 
-    answer: () => `Today's date is ${getTodayDate()}.` 
+  {
+    keywords: ["today", "date", "today's date"],
+    answer: () => `${getRandomGreeting()} Today's date is ${getTodayDate()}.`
   }
 ];
